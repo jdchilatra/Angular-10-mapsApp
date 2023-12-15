c7474946773d0b92344fc1b8433ab64b29dfe00d
david <davidchilatra@gmail.com>
2023-12-14 19:12:46 -0500

    tarjetas de mapas

--- src/app/maps/components/mini-map/mini-map.component.ts
+++ src/app/maps/components/mini-map/mini-map.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, Input, ViewChild } from '@angular/core';
+import { Map, Marker } from 'mapbox-gl';
 
 @Component({
-  selector: 'app-mini-map',
+  selector: 'map-mini-map',
   templateUrl: './mini-map.component.html',
   styleUrls: ['./mini-map.component.css']
 })
-export class MiniMapComponent {
+export class MiniMapComponent implements AfterViewInit {
+
+
+  @ViewChild('map') divMap? : ElementRef
+  @Input() lngLat?: [number,number]
+
+  ngAfterViewInit(): void {
+    if(!this.divMap?.nativeElement) throw 'elementds'
+    if (!this.lngLat) throw 'dfadsfasdfads'
+
+    const map = new Map({
+      container: this.divMap.nativeElement, // container ID
+      style: 'mapbox://styles/mapbox/streets-v12', // style URL
+      center: this.lngLat, // starting position [lng, lat]
+      zoom: 15, // starting zoom
+      interactive: false
+    });
+
+    new Marker()
+      .setLngLat(this.lngLat)
+      .addTo(map);
+  }
+
 
 }
